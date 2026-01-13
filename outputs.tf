fd6bb7e777960be42140ca02638937f9e8e78949
diegoislas30 <diegoenrique.islas@xpertal.com>
2026-01-13 17:00:30 -0600

    se remueve el modulo rg-container-portalcostos-prd

--- outputs.tf
+++ outputs.tf
@@ -13,13 +13,4 @@ output "rg-scxpeicmprd-location" {
   value       = module.rg-scxpeicmprd.resource_group_location
 }
 
-output "rg-container-portalcostos-prd-name" {
-  description = "Nombre del Resource Group"
-  value       = module.rg-container-portalcostos-prd.resource_group_name
-}
-
-output "rg-container-portalcostos-prd-id" {
-  description = "ID del Resource Group"
-  value       = module.rg-container-portalcostos-prd.resource_group_id
-}
 
