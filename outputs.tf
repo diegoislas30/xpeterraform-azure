40c92b700aa5b4c58bb76b3da87d80c9e757fde9
diegoislas30 <diegoenrique.islas@xpertal.com>
2026-01-21 13:15:28 -0600

    se genera rg-test-xpeansible

--- outputs.tf
+++ outputs.tf
@@ -13,4 +13,17 @@ output "rg-scxpeicmprd-location" {
   value       = module.rg-scxpeicmprd.resource_group_location
 }
 
+output "rg-test-xpeansible-name" {
+  description = "Nombre del Resource Group"
+  value       = module.rg-test-xpeansible.resource_group_name
+}
 
+output "rg-test-xpeansible-id" {
+  description = "ID del Resource Group"
+  value       = module.rg-test-xpeansible.resource_group_id
+}
+
+output "rg-test-xpeansible-location" {
+  description = "Location del Resource Group"
+  value       = module.rg-test-xpeansible.resource_group_location
+}
